3fe34a9fc36d1bf4fbfd80bb991740ec1f546f11
Jiwoong Youn <yjw9012job@gmail.com>
2016-08-03 00:20:22 -0700

    Add SearchBoxComponent and SearchResultComponent

--- app/ts/components/YouTubeSearchComponent.ts
+++ app/ts/components/YouTubeSearchComponent.ts
@@ -1,6 +1,6 @@
-import {Component} from "@angular/core";
+import {Component, Injectable, Inject, bind, OnInit, ElementRef, EventEmitter} from "@angular/core";
 import {Http, Response} from "@angular/http";
-import {Observable} from "rxjs/Observable";
+import {Observable} from "rxjs";
 
 class SearchResult {
     id: string;
@@ -19,17 +19,11 @@ class SearchResult {
 }
 
 
-const YOUTUBE_API_KEY: string = "AIzaSyDOfT_BO81aEZScosfTYMruJobmpjqNeEk";
-const YOUTUBE_API_URL: string = "https://www.googleapis.com/youtube/v3/search";
-
-export var youtubeServiceInjectables: any[] = [
-    bind(YoutubeService).toClass(YoutubeService),
-    bind(YOUTUBE_API_KEY).toValue(YOUTUBE_API_KEY),
-    bind(YOUTUBE_API_URL).toValue(YOUTUBE_API_URL)
-];
+export const YOUTUBE_API_KEY: string = "AIzaSyDOfT_BO81aEZScosfTYMruJobmpjqNeEk";
+export const YOUTUBE_API_URL: string = "https://www.googleapis.com/youtube/v3/search";
 
 @Injectable()
-export class YoutubeService {
+export class YouTubeService {
     constructor(public http: Http,
                 @Inject(YOUTUBE_API_KEY) private apiKey: string,
                 @Inject(YOUTUBE_API_URL) private apiUrl: string) {
@@ -59,4 +53,68 @@ export class YoutubeService {
                 });
             });
     }
+}
+
+export var youtubeServiceInjectables: any[] = [
+    bind(YouTubeService).toClass(YouTubeService),
+    bind(YOUTUBE_API_KEY).toValue(YOUTUBE_API_KEY),
+    bind(YOUTUBE_API_URL).toValue(YOUTUBE_API_URL)
+];
+
+@Component({
+    selector: "search-box",
+    outputs: ["loading", "results"],
+    template: `
+        <input type="text" class="form-control" autofocus placeholder="Search">
+    `
+})
+
+export class SearchBoxComponent implements OnInit {
+    loading: EventEmitter<boolean> = new EventEmitter<boolean>();
+    results: EventEmitter<SearchResult[]> = new EventEmitter<SearchResult[]>();
+
+    constructor(public youtube: YouTubeService, private el: ElementRef) {
+
+    }
+
+    ngOnInit(): void {
+        Observable.fromEvent(this.el.nativeElement, "keyup")
+            .map((e: any) => e.target.value)
+            .filter((text: string) => text.length > 1)
+            .debounceTime(250)
+            .do(() => this.loading.next(true))
+            .map((query: string) => this.youtube.search(query))
+            .switch()
+            .subscribe(
+                (results: SearchResult[]) => {
+                    this.loading.next(false);
+                    this.results.next(results);
+                }, (error: any) => {
+                    this.loading.next(false);
+                }, () => {
+                    this.loading.next(false);
+                }
+            );
+    }
+}
+
+@Component({
+    selector: "search-result",
+    inputs: ["result"],
+    template: `
+        <div class="col-sm-6 col-md-3">
+            <div class="thumbnail">
+                <img src="{{result.thumbnailUrl}}">
+                <div class="caption">
+                    <h3>{{result.title}}</h3>
+                    <p>{{result.description}}</p>
+                    <p><a href="{{result.videoUrl}}" class="btn btn-default" role="button">Watch</a></p>
+                </div>
+            </div>
+        </div>
+    `
+})
+
+export class SearchResultComponent {
+    result: SearchResult;
 }
